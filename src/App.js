a436046d7950f0975b9ee602d6fb35cab01289c0
Harikrishnan <hari@Harikrishnans-MacBook-Air.local>
2022-03-24 20:20:29 +0530

    axios installed, backend linked

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-
+import React, {useState} from 'react'
+import axios from 'axios'
 import './App.css';
 
 function App() {
+  const [state,setState]=useState()
+  function click(){
+     axios.get("/new").then((response)=>{
+      console.log(response)
+      setState(response.data)
+     })
+    
+  }
   return (
-    <div className="App">
+    <div  className="App">
       <h1>hello</h1>
+      <button onClick={click}>press</button>
+      <h1>{state}</h1>
+      
     </div>
   );
 }
