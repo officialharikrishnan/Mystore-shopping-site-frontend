bbbf3d8eaa3a1de63fffbf7fb82504c4a32eb129
Harikrishnan <hari@Harikrishnans-MacBook-Air.local>
2022-03-28 21:24:14 +0530

    get value from form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,39 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Container, Row, Col } from 'react-grid';
 import './Login.css'
 import image from '../image/logo.png'
-function Login() {
-  return (
-    <div className='loginpage'>
-      <Container>
-        <Row>
-          <Col md={6}>
-            <p id='logoname'>MY STORE</p>
-            <img id='logo' src={image} alt="" />
-          </Col>
-          <Col md={6}>
-
-            <div id="login-form" >
-              <p>Login</p>
-              <form action="">
-                <input type="text" placeholder='Email or phone' />
-                <input type="password" placeholder='Password' />
-                <a id='forgot' href="">Forgot password</a>  
-                <a id ="create-acc"href="/signup">Create an account</a> 
-                <button id='btn' type="submit">Go!</button>
-                <br />
-              </form>
-            </div>
-
-
-          </Col>
-
-        </Row>
-      </Container>
-
-
-    </div>
-  )
+function Login () {
+  const [phone,setPhone]=useState()
+  const [password,setPassword]=useState()
+ 
+  function handlPhoneChange(e){
+    setPhone({phone: e.target.value})
+  }
+  function handlePasswordChange(e){
+    setPassword({password: e.target.value})
+  }
+  
+  
+  function handleSubmit(e){
+    alert("submited")
+    e.preventDefault();
+    console.log(phone,password)
+  }
+  
+    return (
+      <div className='loginpage'>
+        <Container>
+          <Row>
+            <Col md={6}>
+              <p id='logoname'>MY STORE</p>
+              <img id='logo' src={image} alt="" />
+            </Col>
+            <Col md={6}>
+  
+              <div id="login-form" >
+                <p>Login</p>
+                <form onSubmit={handleSubmit} action="/submit" method='post' encType='application/x-www-form-urlencoded'>
+                  <input onChange={handlPhoneChange}  type="text" name='phone' id='phone' placeholder='Email or phone' />
+                  <input onChange={handlePasswordChange} type="password" name='password' id='password'  placeholder='Password' />
+                  <a id='forgot' href="/">Forgot password</a>  
+                  <a id ="create-acc"href="/signup">Create an account</a> 
+                  <button id='btn' value="Submit" type="submit">Go!</button>
+                  <br />
+                </form>
+              </div>
+  
+  
+            </Col>
+  
+          </Row>
+        </Container>
+  
+  
+      </div>
+    )
+  
+  
 }
 
 export default Login
